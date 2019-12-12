04621f08d9fc04e63756f435475d0ed5987abcc0
bogdan <bshelomanov98@mail.ru>
2019-12-12 15:30:44 +0300

    create emiter

---
+++ src/observable/js/emiter.js
@@ -0,0 +1,100 @@
+class EventEmiter {
+  constructor() {
+    this.subscribers = {};
+    this.operators = [];
+  }
+
+  on(type, listener) {
+    if (!this.subscribers[type]) {
+      this.subscribers[type] = { observers: [] };
+    }
+    this.subscribers[type].observers.push(listener);
+
+    return () => this.remove(type, listener);
+  }
+
+  once(type, listener) {
+    const newlistener = { update: listener, once: true };
+    this.on(type, newlistener);
+    return this;
+  }
+
+  emit(type, data) {
+    if (!this.subscribers[type]) {
+      return;
+    }
+
+    const filtredData = this.setOperators(data);
+
+    if (filtredData || !data) {
+      this.notifyObservers(type, filtredData);
+    }
+  }
+
+  off(type, listener) {
+    this.remove(type, listener);
+    return this;
+  }
+
+  remove(type, listener) {
+    if (this.subscribers[type]) {
+      const index = this.subscribers[type].observers.indexOf(listener);
+      this.subscribers[type].observers.splice(index, 1);
+    }
+
+    if (!listener) {
+      this.subscribers[type].observers = [];
+    }
+
+    return this;
+  }
+
+  notifyObservers(type, filtredData) {
+    const observers = this.subscribers[type].observers;
+
+    for (const observer of observers) {
+      if (typeof observer === "function") {
+        observer(filtredData || {});
+      } else {
+        observer.update(filtredData || {});
+        if (observer.once) {
+          this.remove(type, observer);
+        }
+      }
+    }
+  }
+
+  setOperators(data) {
+    let filtredData = data;
+    for (const operator of this.operators) {
+      if (operator.name === "filter") {
+        if (!operator.fn(data)) return;
+      }
+
+      if (operator.name === "map") {
+        filtredData = operator.fn(data);
+      }
+
+      if (typeof operator === "function") {
+        filtredData = operator(data);
+      }
+    }
+
+    return filtredData;
+  }
+
+  map(callback) {
+    this.operators.push({ name: "map", fn: callback });
+    return this;
+  }
+
+  filter(predicate) {
+    this.operators.push({ name: "filter", fn: predicate });
+    return this;
+  }
+
+  pipe(...operator) {
+    this.operators.push(...operator);
+    return this;
+  }
+}
--- src/observable/js/index.js
+++ src/observable/js/index.js
@@ -1,225 +1,25 @@
-// import {
-//   StreamObservable,
-//   StreamObserver,
-//   StreamObserverSecond,
-// } from './actions'
-// const observer = new StreamObserver()
-// // const observer2 = new StreamObserverSecond()
+const ee = new EventEmiter();
+const listener = data => {
+  console.log("room3 - listener1", data);
+};
+const listener2 = data => {
+  console.log("room3 - listener2", data);
+};
 
-// const observable = new StreamObservable()
+ee.on("room1", data => {
+  console.log("room1", data);
+});
 
-// const foo = () => {
-//   const element = document.querySelectorAll('.product')
-//   element[0].addEventListener('click', () => {
-//     observable.filter().map().subscribe(observer).next('first')
-//   })
-//   element[1].addEventListener('click', () => {
-//     observable.subscribe(observer).next('second')
-//   })
-// }
+ee.once("room2", data => {
+  console.log("room2", data);
+});
 
-// foo()
+ee.on("room3", listener);
+ee.on("room3", listener2);
 
-// const filterCustom = predicate => ({ name: 'filter', fn: predicate })
-// const mapCustom = callback => ({ name: 'map', fn: callback })
+// ee.off("room3"); // off all observer for channel
+ee.off("room3", listener); // off observer [listener]
 
-// const observer = new StreamObserver()
-// const observer2 = new StreamObserverSecond()
-
-// const observable = new StreamObservable()
-// const destination = observable.pipe(
-//   filterCustom(item => true),
-//   mapCustom(item => item.toUpperCase()),
-// )
-
-// destination.subscribe(observer)
-
-// const foo = () => {
-//   const element = document.querySelectorAll('.product')
-//   element[0].addEventListener('click', () => {
-//     observable.next('first')
-//   })
-//   element[1].addEventListener('click', () => {
-//     observable.next('second')
-//   })
-// }
-
-// foo()
-
-// const filterCustom = predicate => ({ name: 'filter', fn: predicate })
-// const mapCustom = callback => ({ name: 'map', fn: callback })
-// const observer = new StreamObserver()
-
-// const observable = new StreamObservable(subscriber => {
-//   subscriber.next('first open constructor')
-// })
-
-// const destination = observable.pipe(
-//   filterCustom(item => true),
-//   mapCustom(item => item.toUpperCase()),
-// )
-
-// destination.subscribe(observer)
-
-// class Store {
-//   getInstance() {
-//     if (!Store.instance) {
-//       return (Store.instance = new Store());
-//     }
-
-//     return Store.instance;
-//   }
-// }
-
-// class Basket {
-//   constructor() {
-//     this.wrapper = document.querySelector(".basket");
-//     this.favoriteProducts = [];
-//     this.observable = null;
-//   }
-
-//   init(id) {
-//     const elements = document.querySelectorAll(".product_remove");
-//     elements.forEach(item => {
-//       item.addEventListener("click", () => {
-//         this.favoriteProducts = this.favoriteProducts.filter(
-//           item => item.id !== id
-//         );
-//         this.render();
-//       });
-//     });
-//   }
-
-//   create(id) {
-//     const element = document.createElement("div");
-//     element.innerHTML = `
-//       <div data-id=${id} class="product_item">
-//         <h3>product ${id}</h3>
-//         <button class="product_remove">remove</button>
-//       </div>
-//     `;
-
-//     this.favoriteProducts.push({ id, element });
-//     this.render();
-//   }
-
-//   render() {
-//     this.wrapper.innerHTML = "";
-//     this.favoriteProducts.forEach(item => {
-//       this.wrapper.appendChild(item.element);
-//     });
-//   }
-
-//   update({ id }) {
-//     console.log("update");
-//     this.create(id);
-//     this.init(id);
-//   }
-// }
-
-// class Products {
-//   constructor(selector) {
-//     this.productsList = document.querySelectorAll(selector);
-//   }
-
-//   init() {
-//     for (const product of this.productsList) {
-//       product.addEventListener("click", () => {
-//         const id = parseInt(product.dataset.id);
-//         observable.next({ id });
-//       });
-//     }
-//   }
-// }
-
-class ProductsObserver {
-  constructor() {
-    this.observable = null;
-    this.favoriteProducts = [];
-    this.wrapper = document.querySelector(".basket");
-  }
-
-  update({ node, type }) {
-    if (type === "add product to basket") {
-      console.log("ProductsObserver: update");
-      this.createNode(node);
-      this.render();
-    }
-  }
-
-  createNode(node) {
-    node.classList.add("isBasket");
-    const actionButton = document.createElement("button");
-    actionButton.classList.add("product_in_basket");
-    actionButton.innerText = "remove";
-    this.favoriteProducts.push(node);
-    node.appendChild(actionButton);
-    this.init(actionButton);
-
-    observable.next({ type: "update node area", nodes: this.favoriteProducts });
-  }
-
-  init(actionButton) {
-    actionButton.addEventListener("click", e => {
-      const node = e.currentTarget.parentNode;
-      this.remove(node);
-    });
-  }
-
-  remove(node) {
-    const index = this.favoriteProducts.indexOf(node);
-    this.favoriteProducts.splice(index, 1);
-    this.render();
-
-    observable.next({ type: "update node area", nodes: this.favoriteProducts });
-  }
-
-  render() {
-    this.wrapper.innerHTML = "";
-    for (const product of this.favoriteProducts) {
-      this.wrapper.appendChild(product);
-    }
-  }
-}
-
-class Products {
-  constructor() {
-    this.products = document.querySelectorAll(".product");
-  }
-
-  init() {
-    for (const product of this.products) {
-      product.addEventListener("click", e => {
-        const node = e.target.parentNode;
-        observable.next({
-          type: "add product to basket",
-          node: node.cloneNode({ deep: true })
-        });
-      });
-    }
-  }
-}
-
-class AreaObserver {
-  constructor() {
-    this.observable = null;
-    this.title = document.querySelector(".title");
-  }
-
-  update({ type, nodes }) {
-    if (type === "update node area") {
-      this.title.innerHTML = "";
-      this.title.innerHTML = nodes.length;
-    }
-  }
-}
-
-const observable = new Observable();
-const productsObserver = new ProductsObserver();
-const areaObserver = new AreaObserver();
-
-const product = new Products();
-product.init();
-
-observable.subscribe(areaObserver);
-observable.subscribe(productsObserver);
+ee.filter(() => true)
+  .map(data => data)
+  .emit("room3", { id: 2 });
