c0b276c0c62115ed2941bddb0758f85a5d69c332
bogdan <bshelomanov98@mail.ru>
2019-12-12 16:48:14 +0300

    create ee children class

--- src/observable/js/index.js
+++ src/observable/js/index.js
@@ -1,25 +1,30 @@
-const ee = new EventEmiter();
-const listener = data => {
-  console.log("room3 - listener1", data);
-};
-const listener2 = data => {
-  console.log("room3 - listener2", data);
-};
+// const ee = new EventEmiter();
 
-ee.on("room1", data => {
-  console.log("room1", data);
-});
+// ee.on("room1", data => {
+//   console.log("room1", data);
+// });
 
-ee.once("room2", data => {
-  console.log("room2", data);
-});
+// ee.filter(() => true)
+//   .map(data => data)
+//   .emit("room1", { data: "some data" });
 
-ee.on("room3", listener);
-ee.on("room3", listener2);
+// const foo = data => data;
+// const getData = data => ({ ...data, newProps: "new data" });
+// ee.pipe(foo, getData).emit("room1", { data: "pipe data" });
 
-// ee.off("room3"); // off all observer for channel
-ee.off("room3", listener); // off observer [listener]
+class Second extends EventEmiter {}
 
-ee.filter(() => true)
-  .map(data => data)
-  .emit("room3", { id: 2 });
+class First extends EventEmiter {
+  init() {
+    const second = new Second();
+    second.on("room1", function() {
+      console.log("room1: trigger", this);
+    });
+
+    second.emit("room1");
+    // second.off("room1");
+    second.emit("room1");
+  }
+}
+
+new First().init();
